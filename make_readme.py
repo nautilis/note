0f25b35b02b2f6e24953308e0e6a9ce24257212a
nautilis <bevanzpf@gmail.com>
2018-01-17 15:25:42 +0800

    make readme file automatically

---
+++ make_readme.py
@@ -0,0 +1,34 @@
+#! /usr/bin/python
+import os
+import re
+
+dirnames = []
+results = [] 
+dirs = os.listdir(".")
+for dir in dirs:
+    if dir[0] == "." or os.path.isfile(dir):
+        continue
+    dirnames.append(dir)
+
+for dir in dirnames:
+    files = os.listdir("./{}/".format(dir))
+    result = {dir: files}
+    results.append(result)
+
+print results
+
+string = ""
+for result in results:
+    for key, values in result.iteritems():
+        string += "## {}  \r\n".format(key)
+        for value in values:
+            string += "- [{}](https://github.com/nautilis/note/blob/master/{}/{})  \r\n".format(value, key, value)
+
+print string
+
+with open ("./readme.md", "w")as f:
+    f.write(string)
+    
+
+
+
---
+++ readme.md
@@ -0,0 +1,7 @@
+## forn-end  
+- [20180116_react.md](https://github.com/nautilis/note/blob/master/forn-end/20180116_react.md)  
+## spider  
+- [20180116_casperjs.md](https://github.com/nautilis/note/blob/master/spider/20180116_casperjs.md)  
+## linux  
+- [20180115_设置终端位置及大小.md](https://github.com/nautilis/note/blob/master/linux/20180115_设置终端位置及大小.md)  
+- [20180116_权限.md](https://github.com/nautilis/note/blob/master/linux/20180116_权限.md)  
